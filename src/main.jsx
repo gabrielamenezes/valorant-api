150555276f8dc1c949dc3e0a1726edd9f4e389dc
Gabriela Menezes <gabimenezess10@gmail.com>
2023-11-30 22:21:27 -0300

    agent provider add to tree

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,21 @@ import Home from './pages/Home.jsx'
 import App from './App.jsx'
 import Favoritos from './pages/Favoritos.jsx'
 import FavoritosProvider from './context/Favoritos.jsx'
+import AgentesProvider from './context/Agentes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <FavoritosProvider>
-        <Routes>
-          <Route element={<App/>}>
-            <Route path="/" element={<Home/>}/>
-            <Route path="favoritos" element={<Favoritos/>}/>
-          </Route>
-        </Routes>
-      </FavoritosProvider>
+      <AgentesProvider>
+        <FavoritosProvider>
+          <Routes>
+            <Route element={<App/>}>
+              <Route path="/" element={<Home/>}/>
+              <Route path="favoritos" element={<Favoritos/>}/>
+            </Route>
+          </Routes>
+        </FavoritosProvider>
+      </AgentesProvider>
      
     </BrowserRouter>
   </React.StrictMode>,
