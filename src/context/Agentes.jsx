24fba0b4f3ce546356eff951e851546fb6f1980d
Gabriela Menezes <gabimenezess10@gmail.com>
2023-11-30 22:15:39 -0300

    feat: create agent context to share agent state

---
+++ src/context/Agentes.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from "react";
+
+export const AgentesContext = createContext();
+AgentesContext.displayName = 'Agentes'
+
+export default function AgentesProvider({children}) {
+    const [agentes, setAgentes] = useState([]);
+
+    
+    return (
+        <AgentesContext.Provider value={{agentes, setAgentes}}>
+            {children}
+        </AgentesContext.Provider>
+    )
+}
+
+export function useAgentesContext() {
+    const {agentes, setAgentes} = useContext(AgentesContext);
+    async function getAgentsAPI(url) {
+        const response = await fetch(url)
+        const data = await response.json();
+        setAgentes(data.data)
+    }
+
+    return {
+        agentes,
+        setAgentes,
+        getAgentsAPI
+    }
+}
