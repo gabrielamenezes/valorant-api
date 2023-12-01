92ab695384fbbb59f821b7194f1b57522b4cb5b0
Gabriela Menezes <gabimenezess10@gmail.com>
2023-11-30 22:22:26 -0300

    remove api call and using customize hook useAgentesContext to show agents data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import AgentCard from "../components/AgentCard";
 import styled from "styled-components";
+import { useAgentesContext } from "../context/Agentes";
 
 const CardContainer = styled.div`
     display: flex;
@@ -10,20 +11,17 @@ const CardContainer = styled.div`
     margin-top: 2rem;
 `
 const Home = () => {
-    const [agents, setAgents] = useState([])
-    const urlGetAgent = `https://valorant-api.com/v1/agents?isPlayableCharacter=true&language=pt-BR`
-    async function getAgents(url) {
-    const response = await fetch(url)
-    const data = await response.json();
-    console.log(data.data)
-    setAgents(data.data)
-  }
+  const {agentes, getAgentsAPI} = useAgentesContext();
+
+  const urlGetAgent = `https://valorant-api.com/v1/agents?isPlayableCharacter=true&language=pt-BR`
+
+    
   useEffect(() => {
-    getAgents(urlGetAgent);
+    getAgentsAPI(urlGetAgent);
   }, []) 
     return (
         <CardContainer>
-            {agents && agents.map(agent => <AgentCard key={agent.uuid} agent={agent}></AgentCard>)}
+            {agentes && agentes.map(agent => <AgentCard key={agent.uuid} agent={agent}></AgentCard>)}
         </CardContainer>
     )
 }
